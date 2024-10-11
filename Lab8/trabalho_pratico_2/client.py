2d5c505d90fd498809c57fa423b45447ab1fdfe3
ronaldinho Vega Centeno <dinho15971@gmail.com>
2024-10-11 19:31:16 -0300

    Lab8 final

---
+++ Lab8/trabalho_pratico_2/client.py
@@ -0,0 +1,105 @@
+import splitlearning_pb2 as pb2
+import splitlearning_pb2_grpc as pb2_grpc
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import cifar10
+import grpc
+import time
+import numpy as np
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+
+def create_partial_model_m1(input_layer):
+    layer1 = layers.Conv2D(32, (3, 3), activation='relu')(input_layer)
+    layer2 = layers.MaxPooling2D((2, 2))(layer1)
+    return Model(inputs=input_layer, outputs=layer2)
+
+def create_partial_model_m3(input_layer):
+    layer5 = layers.Dense(64, activation='relu')(input_layer) 
+    layer6 = layers.Dense(10, activation='softmax')(layer5) 
+    return Model(inputs=input_layer, outputs=layer6)
+
+def get_activations(model, X):
+    with tf.GradientTape(persistent=True) as tape:
+        activations = model(X)
+    return activations, tape
+
+def train_step(model_m1, model_m3, x_batch, y_batch, batch, optimizer, epoch, stub):
+
+    with tf.GradientTape(persistent=True) as tape_m1:
+
+        activations_m1 = model_m1(x_batch, training = True )
+        flattened_activations_m1 = tf.reshape(activations_m1, (activations_m1.shape[0], -1))
+        # Enviar ativações ao servidor para processar M2
+        activations_list = flattened_activations_m1.numpy().flatten()
+        activation_message = pb2.ActivationsRequest()
+        activation_message.activations.extend(activations_list)
+        response = stub.SendActivation(activation_message)
+        # Receber as ativações processadas de M2
+        activations_m2 = tf.convert_to_tensor(response.activations, dtype=tf.float32)
+        activations_m2 = tf.reshape(activations_m2, (64, 128)) 
+
+        # Forward pass no cliente (M3)
+        with tf.GradientTape(persistent=True) as tape_m2:
+            tape_m2.watch(activations_m2)
+            predictions = model_m3(activations_m2, training = True)
+            loss        = tf.keras.losses.sparse_categorical_crossentropy(y_batch, predictions)
+            loss        = tf.reduce_mean(loss)
+
+    # Backpropagation no cliente (M3)
+    grads_m3 = tape_m2.gradient(loss, model_m3.trainable_variables)
+    optimizer.apply_gradients(zip(grads_m3, model_m3.trainable_variables))
+    
+    # **Aqui, calcular os gradientes das ativações de M2**
+    # Esses gradientes são o que devemos enviar ao servidor
+    grads_activations_m2 = tape_m2.gradient(loss, activations_m2)
+    # Enviar gradientes das ativações de M2 para o servidor processar o backpropagation em M2
+    gradient_message = pb2.GradientsRequest()
+    gradient_message.gradients.extend(grads_activations_m2.numpy().flatten())
+    response2 = stub.SendGradient(gradient_message)
+    # Opcional: Receber gradientes para M1 e atualizar pesos no cliente (M1)
+    activations_grad = tf.convert_to_tensor(response2.gradients, dtype=tf.float32)
+    activations_grad = tf.reshape(activations_grad, (64, 15, 15, 32))
+    gradients_M1 = tape_m1.gradient(activations_m1, model_m1.trainable_variables, output_gradients=activations_grad)
+
+    optimizer_m1 = tf.keras.optimizers.Adam()
+    optimizer_m1.apply_gradients(zip(gradients_M1, model_m1.trainable_variables))
+    
+
+def main():
+
+    # CIFAR-10 dataset
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    X_train, X_test                      = X_train / 255.0, X_test / 255.0    
+    
+    partial_model_m1     = create_partial_model_m1(layers.Input(shape=(32, 32, 3)))
+    partial_model_m3     = create_partial_model_m3(layers.Input(shape=(128,)))
+
+    client_optimizer   = tf.keras.optimizers.Adam()
+    MAX_MESSAGE_LENGTH = 20 * 1024 * 1024 *10
+    
+    # Configuração da conexão gRPC
+    channel = grpc.insecure_channel('localhost:50051',options=[
+            ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+            ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+        ])
+    stub = pb2_grpc.SplitLearningStub(channel)
+
+    for epoch in range(10):
+        batch_size = 64
+        n_batches  = X_train.shape[0]//batch_size
+        
+        for batch in range(n_batches):
+            print(f"Epoch {epoch} - Batch {batch}/{n_batches}")
+            
+            X_batch  = X_train[batch_size * batch : batch_size * (batch+1)]
+            y_batch  = y_train[batch_size * batch : batch_size * (batch+1)]
+
+            train_step(partial_model_m1, partial_model_m3, X_batch, y_batch, batch, client_optimizer, epoch, stub)
+        
+    
+if __name__ == '__main__':
+    main()
---
+++ Lab8/trabalho_pratico_2/server.py
@@ -0,0 +1,109 @@
+import splitlearning_pb2 as pb2
+import splitlearning_pb2_grpc as pb2_grpc
+import grpc
+from concurrent import futures
+import tensorflow as tf
+from  tensorflow.keras import layers, models
+from keras.models import Model
+import time
+import pandas as pd
+import os
+import numpy as np
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+loss_to_save = []
+epoch = 0
+
+class ServerModel(tf.keras.models.Model):
+    def __init__(self):
+        super(ServerModel, self).__init__()
+        self.layer3 = layers.Conv2D(64, (3, 3), activation='relu')
+        self.flatten = layers.Flatten() 
+        self.layer4 = layers.Dense(128, activation='relu')
+        self.tape = None
+    
+    def call(self, input):
+        x = self.layer3(input)
+        x = self.flatten(x)
+        x = self.layer4(x)
+        return x
+    
+def create_server_model(input_shape):
+    return ServerModel()
+
+class SplitLearningService(pb2_grpc.SplitLearningServicer):
+
+    def __init__(self):
+        self.server_model = create_server_model((15,15,32))
+        self.optimizer    = tf.keras.optimizers.Adam()
+        self.metrics      = tf.keras.metrics.SparseCategoricalAccuracy()
+        self.last_input = None 
+
+    def SendActivation(self, request, context):
+
+        # Recebe ativações do cliente
+        activations = tf.convert_to_tensor(request.activations, dtype=tf.float32)
+        activations = tf.reshape(activations, (64, 15, 15, 32)) 
+
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(activations)
+            # Realiza feedforward em M2
+            activations_m2 = self.server_model(activations, training = True)
+            # Envia ativações de M2 de volta ao cliente
+            
+        self.tape = tape
+        self.last_input = activations
+        response = pb2.ActivationsResponse()
+        response.activations.extend(activations_m2.numpy().flatten())
+        return response
+
+    def SendGradient(self, request, context):
+        # Recebe gradientes do cliente
+        activations_grad = tf.convert_to_tensor(request.gradients, dtype=tf.float32)
+        activations_grad = tf.reshape(activations_grad, (64, 128))
+
+        # Atualiza o modelo M2 com o backpropagation usando os gradientes
+        with self.tape:
+            server_outputs = self.server_model(self.last_input, training=True)
+            gradients = self.tape.gradient(
+                server_outputs, self.server_model.trainable_variables, output_gradients=activations_grad
+            )
+
+        
+        
+        # Aplica os gradientes para atualizar os pesos de M2
+        self.optimizer.apply_gradients(zip(gradients, self.server_model.trainable_variables))
+
+        # Agora, calcule os gradientes das ativações de M1 (para enviar de volta ao cliente)
+        # Isso permite que o cliente continue o backpropagation em M1
+        activation_gradients = self.tape.gradient(
+            server_outputs, self.last_input, output_gradients=activations_grad
+        )
+        response = pb2.GradientsResponse()
+
+        response.gradients.extend(activation_gradients.numpy().flatten())
+        # Enviar gradientes das ativações de M1 de volta ao cliente
+        return response
+
+
+        
+
+# Função principal para iniciar o servidor gRPC
+def serve():
+    global server
+    MAX_MESSAGE_LENGTH = 20 * 1024 * 1024 * 10
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+            ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+        ])
+    pb2_grpc.add_SplitLearningServicer_to_server(SplitLearningService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server started on port 50051")
+    server.wait_for_termination()
+    
+
+if __name__ == '__main__':
+    serve()
